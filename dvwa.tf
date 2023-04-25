6e0f95d2bab060c8ad0aa0e82d16c7e1aaca731d
DarkRelay <94099510+DarkRelayLabs@users.noreply.github.com>
2023-04-25 18:55:30 +0530

    Update dvwa.tf

--- dvwa.tf
+++ dvwa.tf
@@ -4,8 +4,7 @@ provider "aws" {
   region     = "us-east-1"
 }
 
-data "template_file" "install_script" {
-  /* File template for the install script */
+data "dvwa_file" "dvwa_script" {
   template = "${file("dvwa.sh")}"
 }
 
@@ -48,7 +47,7 @@ resource "aws_instance" "dvwa" {
   
   
   # Configure the user data to install DVWA on the instance
-  user_data = "${data.template_file.install_script.rendered}"
+  user_data = "${data.dvwa_file.dvwa_script.rendered}"
 
   tags = {
     Name = "DVWA Instance"
