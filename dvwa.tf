82e11ccf1edf332fa2bbd1d0014d25d63a6dc6db
DarkRelay <94099510+DarkRelayLabs@users.noreply.github.com>
2023-04-25 18:53:32 +0530

    Add files via upload

---
+++ dvwa.tf
@@ -0,0 +1,57 @@
+provider "aws" {
+  access_key = "IAMACCESSKEY"
+  secret_key = "IAMSECRETKEY"
+  region     = "us-east-1"
+}
+
+data "template_file" "install_script" {
+  /* File template for the install script */
+  template = "${file("dvwa.sh")}"
+}
+
+resource "aws_security_group" "dvwa_sg" {
+  name_prefix = "dvwa_sg"
+  description = "Security group for DVWA instance"
+
+  ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = {
+    Name = "dvwa_sg"
+  }
+}
+
+resource "aws_instance" "dvwa" {
+  ami           = "ami-007855ac798b5175e" # Specify the AMI ID for your desired Ubuntu version.
+  instance_type = "t2.micro"             # Specify the instance type.
+  key_name      = "dvwa_key"        # Specify the name of the key pair you want to use to connect to the instance.
+
+  vpc_security_group_ids = [aws_security_group.dvwa_sg.id]
+  
+  
+  # Configure the user data to install DVWA on the instance
+  user_data = "${data.template_file.install_script.rendered}"
+
+  tags = {
+    Name = "DVWA Instance"
+  }
+
+}
