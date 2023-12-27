8dfa5f3b94e814765f63ee21b834c314e7c6199a
DarkRelay <94099510+DarkRelayLabs@users.noreply.github.com>
2023-12-27 09:48:23 +0530

    Update vulnlab.tf

--- vulnlab.tf
+++ vulnlab.tf
@@ -109,7 +109,7 @@ resource "aws_security_group" "vulnlab_sg" {
     from_port   = 111
     to_port     = 111
     protocol    = "tcp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+    cidr_blocks = ["0.0.0.0/0"]
   }
   
   ingress {
@@ -153,13 +153,6 @@ resource "aws_security_group" "vulnlab_sg" {
     protocol    = "tcp"
      cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
-
-  ingress {
-    from_port   = -1  # ICMP type -1 represents all ICMP traffic
-    to_port     = -1
-    protocol    = "icmp"
-    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
-  }
   
   egress {
     from_port   = 0
@@ -188,4 +181,30 @@ resource "aws_security_group" "vulnlab_sg" {
   tags = {
     Name = "vulndocker"
   }
+}
+
+resource "aws_internet_gateway" "vuln_igw" {
+  vpc_id = aws_vpc.vuln_vpc.id
+
+  tags = {
+    Name = "vuln-igw"
+  }
+}
+
+resource "aws_route_table" "vuln_route_table" {
+  vpc_id = aws_vpc.vuln_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.vuln_igw.id
+  }
+
+  tags = {
+    Name = "vuln-route-table"
+  }
+}
+
+resource "aws_route_table_association" "vuln_route_association" {
+  subnet_id      = aws_subnet.vuln_subnet.id
+  route_table_id = aws_route_table.vuln_route_table.id
 }
