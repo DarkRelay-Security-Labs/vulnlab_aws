faddc45c77f7ba59d6bc1a916b480ebbd314c94a
DarkRelay <94099510+DarkRelayLabs@users.noreply.github.com>
2024-01-19 17:24:56 +0530

    Update vulnlab.tf

--- vulnlab.tf
+++ vulnlab.tf
@@ -49,6 +49,13 @@ resource "aws_security_group" "vulnlab_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+ingress {
+    from_port   = 25
+    to_port     = 25
+    protocol    = "tcp"
+     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -80,6 +87,132 @@ resource "aws_security_group" "vulnlab_sg" {
   ingress {
     from_port   = 85
     to_port     = 85
+    protocol    = "tcp"
+     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+ingress {
+    from_port   = 111
+    to_port     = 111
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 137
+    to_port     = 137
+    protocol    = "udp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 138
+    to_port     = 138
+    protocol    = "udp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 139
+    to_port     = 139
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 161
+    to_port     = 161
+    protocol    = "udp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 199
+    to_port     = 199
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 389
+    to_port     = 389
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+   ingress {
+    from_port   = 443
+    to_port     = 443
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 445
+    to_port     = 445
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 636
+    to_port     = 636
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 2049
+    to_port     = 2049
+    protocol    = "tcp"
+    cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 4444
+    to_port     = 4444
+    protocol    = "tcp"
+     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 4567
+    to_port     = 4567
+    protocol    = "tcp"
+     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 4568
+    to_port     = 4568
+    protocol    = "tcp"
+     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+
+  ingress {
+    from_port   = 6200
+    to_port     = 6200
+    protocol    = "tcp"
+     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 6306
+    to_port     = 6306
+    protocol    = "tcp"
+     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 8080
+    to_port     = 8080
+    protocol    = "tcp"
+     cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
+  }
+  
+  ingress {
+    from_port   = 9306
+    to_port     = 9306
     protocol    = "tcp"
      cidr_blocks = ["${chomp(data.http.myip.response_body)}/32"]
   }
